c154473a6866a5fbe37c17d0f05f9ff6c5cebc43
trupesh1710 <trupeshprajapati123@gmail.com>
2025-08-05 13:45:05 +0530

    Project structure ready and venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Trupesh Prajapati",
+    author_email="youcanuseanywhere@gmail.com",
+    packages=find_packages()
+)
